2fc996e322d599bb716a38114942bddfa33c4905
Dan Bailey <dbailey@danbailey.net>
2023-05-24 12:47:33 -0700

    loopstudios

--- big-projects/loopstudios/tailwind.config.js
+++ big-projects/loopstudios/tailwind.config.js
@@ -9,9 +9,14 @@ module.exports = {
       xl: '1440px'
     },
     extend: {
+      colors: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      },
       fontFamily: {
-        sans: ['Josefin Sans', 'sans-serif']
-        alata: ['Alata'],
+        sans: ['Bai Jamjuree', 'sans-serif'],
       },
     },
   },
